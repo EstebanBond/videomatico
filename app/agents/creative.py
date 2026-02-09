3c11eaa8bf49f608a359dbbf3bfc0f5b09d0b12e
Victor Rojano <esteban.rojano.d@gmail.com>
2026-02-09 17:04:51 -0600

    correction en agente creativ0 actualización de version de modelo

--- app/agents/creative.py
+++ app/agents/creative.py
@@ -5,7 +5,7 @@ from app.models.state import GraphState, VideoProjectSchema
 # Inicializamos modelo de LangChain
 
 llm = ChatGoogleGenerativeAI(
-    model="gemini-1.5-flash",
+    model="gemini-2.5-flash",
     temperature=0.7,
     google_api_key=os.getenv("GOOGLE_API_KEY")
 )
